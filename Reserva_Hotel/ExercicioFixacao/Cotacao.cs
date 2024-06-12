eef6ca06cc462f23de43436d3437b6c484856753
Ghesa <124006021+KaioGhesa@users.noreply.github.com>
2024-06-12 13:17:00 -0300

    Add files via upload

---
+++ Reserva_Hotel/ExercicioFixacao/Cotacao.cs
@@ -0,0 +1,15 @@
+﻿namespace ExercicioFixacao
+{
+    internal class Cotacao
+    {
+
+        public static double IOF = 6.0;
+
+        public static double DolarParaReal(double quantia , double cotacao)
+        {
+            double total = quantia * cotacao;
+            return total + total * IOF / 100;
+        }
+
+    }
+}
---
+++ Reserva_Hotel/ExercicioFixacao/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Globalization;
+
+namespace ExercicioFixacao
+{
+    internal class Program
+    {
+        Cotacao cotacao = new Cotacao();
+
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação do Dolar? ");
+            double cotacao = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantos dolares você quer comprar? ");
+            double quantia = double.Parse(Console.ReadLine());
+
+            double result = Cotacao.DolarParaReal(cotacao, quantia);
+
+            Console.WriteLine("Valor a ser pago em reais: " + result.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
